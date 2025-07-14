2682aa974f6d752827e18cc82c7b38d28697aa23
Simran Verma <simranverma.sv17@gmail.com>
2025-07-14 23:06:48 +0530

    index file for promise error handling.

---
+++ promiseErrorHandle.js
@@ -0,0 +1,26 @@
+function fetchData(success = true) {
+  return new Promise((resolve, reject) => {
+    console.log("Fetching data.........");
+
+    setTimeout(() => {
+      if (success) {
+        resolve("Data fetched successfully!");
+      } else {
+        reject("Failed to fetch data.");
+      }
+    }, 1500);
+  });
+}
+
+
+function runFetch(success) {
+  fetchData(success)
+    .then((data) => {
+      console.log("THEN block runs:");
+      console.log(data);
+    })
+    .catch((error) => {
+      console.log("CATCH block runs:");
+      console.error(error);
+    });
+}
